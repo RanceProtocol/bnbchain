f2d93927a082cfe7d1175742c478e91e9002192d
AjayiMike <ajayimikeofficial@gmail.com>
2022-06-28 23:24:25 +0100

    add useTraansaction hook

--- hooks/useTransaction.ts
+++ hooks/useTransaction.ts
@@ -1,1 +1,37 @@
-export default {}
+import { ethers } from "ethers";
+
+export interface SendParams {
+    method: ethers.ContractFunction;
+    methodParams?: any[];
+    callbacks?: { [key: string]: () => void };
+}
+
+const useTransaction = () => {
+    const send = async (params: SendParams) => {
+        const { method, methodParams, callbacks } = params;
+
+        try {
+            const txResponse = await method(
+                ...(methodParams ? methodParams : [])
+            );
+            callbacks && callbacks["sent"] && callbacks["sent"]();
+
+            const txReceipt = await txResponse.wait();
+
+            if (txReceipt.status === 1) {
+                callbacks &&
+                    callbacks["successfull"] &&
+                    callbacks["successfull"]();
+            } else {
+                callbacks && callbacks["failed"] && callbacks["failed"]();
+            }
+        } catch (error: any) {
+            console.error(error);
+            callbacks && callbacks["failed"] && callbacks["failed"]();
+        }
+    };
+
+    return {send}
+};
+
+export default useTransaction;
